bf6b20ef712e1e701b3873d8cbc616a0a7fbf69a
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-08 11:29:23 +0100

    chore Setting layout

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,33 +1,38 @@
-import Head from "next/head"
-import { Container } from "react-bulma-components";
-import Header from "./Header";
+
+import { motion } from 'framer-motion'
 
 
 
-
+const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+}
 
 
 
 const Layout = ({ children }) => {
 
-    return <>
+    return <div>
 
-        <Head>
 
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <Header />
-        <main>
+        <motion.main
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ type: 'linear' }}
+            className="
+                    flex flex-col items-start w-full pt-10
+                    px-8 sm:px-16 md:px-36 lg:px-52 xl:px-80 2xl:px-96
+                    pt-24 h-full
+                "
+        >
             {children}
-        </main>
-        <footer>
-            <Container>
+        </motion.main>
 
-                <p>Copyright @ M.A.S</p>
-            </Container>
-        </footer>
 
-    </>
+    </div>
 
 
 }

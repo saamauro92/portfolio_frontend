c099a1452261cf331d230d89d0ee0a51f07b587f
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-08 11:30:40 +0100

    chore: setting layout and animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
 import Layout from '../components/_App/Layout'
 import { fetchAPI } from '../lib/api'
 import '../styles/globals.scss'
+import { AnimatePresence } from 'framer-motion'
+
+import Header from '../components/_App/Header'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
 
 
     <>
-      <Layout>
-
+      <Header />
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
         <Component {...pageProps} />
-      </Layout>
+      </AnimatePresence>
+
+      <Footer />
+
 
     </>
 

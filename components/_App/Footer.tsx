d564fb015c6efab0e6ab04f6780d1819c4c0fc7e
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-22 21:15:34 +0100

    chore: adding social media

---
+++ components/_App/Footer.tsx
@@ -0,0 +1,29 @@
+import SocialMedia from "components/SocialMedia";
+import { Section } from "react-bulma-components";
+
+const FooterSection = (): JSX.Element => {
+  return (
+    <>
+      <Section mt={6}>
+        <footer className="has-text-centered">
+          <SocialMedia />
+
+          <p className="has-text-centered has-text-white has-text-weight-light p-4">
+            Site written using the ReactJS framework NextJS with Typescript,
+            Bulma CSS and Strapi as CMS.
+            <a
+              className="has-text-secondary is-underlined"
+              href="https://github.com/saamauro92/portfolio_frontend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Source Code
+            </a>
+          </p>
+        </footer>
+      </Section>
+    </>
+  );
+};
+
+export default FooterSection;

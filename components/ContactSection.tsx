aede9364c999dcc07cf107912f4f1134dd44e07c
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-30 23:11:37 +0200

    feat: contact form working

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,77 +1,181 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
-import { Columns, Form, Section, Icon, Button } from "react-bulma-components";
+import React, { useState, useReducer } from "react";
+import {
+  Columns,
+  Form,
+  Section,
+  Button,
+  Block,
+  Heading,
+} from "react-bulma-components";
+import emailjs from "emailjs-com";
+
+type DataTypes = {
+  [x: string]: any;
+};
+
+const service = process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE as string;
+const template = process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE as string;
+const userId = process.env.NEXT_PUBLIC_EMAIL_JS_USER as string | undefined;
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+
+function reducer(state: DataTypes, action: any) {
+  switch (action.type) {
+    case "name":
+      return { ...state, name: action.value };
+    case "email":
+      return { ...state, email: action.value };
+    case "message":
+      return { ...state, message: action.value };
+    default:
+      throw new Error();
+  }
+}
+
+const { Label, Help, Control, Input, Textarea, Field } = Form;
 const { Column } = Columns;
 
 const ContactSection = (): JSX.Element => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [formState, dispatch] = useReducer(reducer, initialState);
+  const { name, email, message } = formState as DataTypes;
+  const [showFormErr, setShowFormErr] = useState(false);
+  const [showFormOrSuccess, setShowFormOrSuccess] = useState(false);
+  const [loadingSpinner, setLoadingSpinner] = useState(false);
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoadingSpinner(true);
+    const params = {
+      ...formState,
+    };
+
+    if (name === "" || email === "" || message === "") {
+      setShowFormErr(true);
+      return;
+    }
+
+    emailjs.send(service, template, params, userId).then(
+      ({ status }) => {
+        if (status === 200) {
+          setShowFormOrSuccess(true);
+        } else {
+          console.log(" Opps, something went wrong, please try again!");
+        }
+      },
+      (err) => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        console.log(" Opps, something went wrong, please try again!");
+      }
+    );
+  };
 
   return (
     <>
       <Section mt={6} mb={6}>
-        <Columns>
-          <Column desktop={{ size: 8 }} mobile={{ size: 12 }}>
-            <form>
-              <Form.Field>
-                <Form.Label className="has-text-white">Name</Form.Label>
-                <Form.Control>
-                  <Form.Input
-                    color="danger"
-                    value={name}
-                    onChange={(e) => {
-                      return setName(e.target.value);
-                    }}
-                  />
-
-                  <Icon align="right" size="small">
-                    <i className="fas fa-exclamation-triangle" />
-                  </Icon>
-                </Form.Control>
-                <Form.Help color="danger has-text-white">
-                  This name is invalid
-                </Form.Help>
-              </Form.Field>
-
-              <Form.Field>
-                <Form.Label className="has-text-white">Email</Form.Label>
-                <Form.Control>
-                  <Form.Input
-                    color="danger"
-                    value={email}
-                    onChange={(e) => {
-                      return setEmail(e.target.value);
-                    }}
-                  />
-
-                  <Icon align="right" size="small">
-                    <i className="fas fa-exclamation-triangle" />
-                  </Icon>
-                </Form.Control>
-                <Form.Help color="danger has-text-white">
-                  This email is invalid
-                </Form.Help>
-              </Form.Field>
-
-              <Form.Field>
-                <Form.Label className="has-text-white">Message</Form.Label>
-                <Form.Textarea
-                  value={message}
-                  onChange={(e) => {
-                    return setMessage(e.target.value);
-                  }}
-                />
-              </Form.Field>
-
-              <Form.Field kind="group">
-                <Form.Control>
-                  <Button color="success">Send Message</Button>
-                </Form.Control>
-              </Form.Field>
-            </form>
-          </Column>
-        </Columns>
+        {showFormOrSuccess ? (
+          <Columns pb={6} mb={6}>
+            <Column desktop={{ size: 8 }} mobile={{ size: 12 }} pb={6} mb={6}>
+              <Block>
+                <Heading subtitle className="has-text-white">
+                  Thank you! Your message has been sent.
+                </Heading>
+              </Block>
+            </Column>
+          </Columns>
+        ) : (
+          <Columns>
+            <Column desktop={{ size: 8 }} mobile={{ size: 12 }}>
+              <form onSubmit={handleOnSubmit}>
+                <Field>
+                  <Label className="has-text-white" htmlFor="contact-form-name">
+                    Name
+                  </Label>
+                  <Control>
+                    <Input
+                      id="contact-form-name"
+                      type="text"
+                      name="user_name"
+                      value={name}
+                      onChange={(e) =>
+                        dispatch({ type: "name", value: e.target.value })
+                      }
+                      maxLength={40}
+                      minLength={2}
+                      required
+                    ></Input>
+                  </Control>
+                </Field>
+                <Field>
+                  <Control>
+                    <Label
+                      className="has-text-white"
+                      htmlFor="contact-form-email"
+                    >
+                      Email
+                    </Label>
+                    <Input
+                      id="contact-form-email"
+                      type="email"
+                      name="user_email"
+                      value={email}
+                      onChange={(e) =>
+                        dispatch({ type: "email", value: e.target.value })
+                      }
+                      required
+                    ></Input>
+                  </Control>
+                </Field>
+                <Field>
+                  <Control>
+                    <Label
+                      className="has-text-white"
+                      htmlFor="contact-form-message"
+                    >
+                      Message
+                    </Label>
+                    <Textarea
+                      id="contact-form-message"
+                      name="message"
+                      value={message}
+                      onChange={(e) =>
+                        dispatch({ type: "message", value: e.target.value })
+                      }
+                      minLength={5}
+                      required
+                    >
+                      {" "}
+                    </Textarea>
+                  </Control>
+                  <Help>
+                    {" "}
+                    {showFormErr ? (
+                      <p className="has-text-white ">
+                        Please fill in all three input boxes to send a message
+                      </p>
+                    ) : null}
+                  </Help>
+                </Field>
+                <Field>
+                  <Control>
+                    <Button
+                      color="success"
+                      type="submit"
+                      className={loadingSpinner ? "is-loading" : ""}
+                    >
+                      SEND
+                    </Button>
+                  </Control>
+                </Field>
+              </form>
+            </Column>
+          </Columns>
+        )}
       </Section>
     </>
   );
